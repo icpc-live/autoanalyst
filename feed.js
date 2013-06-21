84abae5a01e95f6c25bad05b2a288099591bfb26
Greg Hamerly <hamerly@cs.baylor.edu>
2013-06-21 16:31:32 -0500

    Removed static data that should be in the database. Added stubs for ability to tweet. Changed design of feed rows a bit.

--- feed.js
+++ feed.js
@@ -25,123 +25,6 @@ var GLOBAL_FEED_ID_COUNTER = 0;
 // how long it's been since the last feed update).
 function currentTimeSeconds() { return Math.floor(new Date().getTime() / 1000); }
 
-// for 2012 wf teams -- quick hack
-var id_to_school_name = {
-'95': 'U New Sout',
-'87': 'U Canterbu',
-'81': 'UF Paraná',
-'50': 'PUCP',
-'77': 'UNI',
-'79': 'UFCG',
-'29': 'ITA',
-'80': 'UF Pernamb',
-'34': 'ITESM-Pueb',
-'74': 'Uniandes',
-'78': 'UTM',
-'30': 'INTEC',
-'73': 'UH',
-'75': 'UNAL Bogot',
-'96': 'IME-USP',
-'76': 'UNC-FaMAF',
-'71': 'U Buenos A',
-'72': 'U de Guana',
-'82': 'UF Rio de ',
-'35': 'Jagielloni',
-'11': 'Charles U ',
-'45': 'NTU KhPI',
-'86': 'U Buchares',
-'63': 'Taurida NU',
-'83': 'UP Catalun',
-'61': 'SPbSU ITMO',
-'42': 'Moscow Sta',
-'8': 'Belarusian',
-'53': 'Saratov St',
-'60': 'St. Peters',
-'41': 'MIPT',
-'107': 'Ural FU',
-'3': 'Altai STU',
-'70': 'Ufa SATU',
-'7': 'BSUIR',
-'48': 'UNN',
-'93': 'Latvia',
-'69': 'Udmurt SU',
-'67': 'TSU',
-'36': 'Kazakh-Bri',
-'1': 'Aalto U',
-'14': 'TU Delft',
-'108': 'Volgograd ',
-'16': 'TUE',
-'51': 'PUT',
-'106': 'U Wroclaw',
-'103': 'U Warsaw',
-'100': 'U Tokyo',
-'44': 'National T',
-'49': 'Peking U',
-'109': 'Wuhan U',
-'68': 'Tsinghua U',
-'17': 'Fudan U',
-'6': 'BUPT',
-'56': 'SJTU',
-'111': 'ZJUT',
-'27': 'IIT Madras',
-'23': 'HKUST',
-'112': 'Zhongshan ',
-'25': 'IIT Delhi',
-'38': 'Kyoto U',
-'59': 'SEI - ECNU',
-'54': 'Seoul Nati',
-'31': 'IIIT Hyder',
-'21': 'GUCAS',
-'5': 'BUET',
-'55': 'SUST',
-'37': 'Korea U',
-'46': 'NUDT',
-'28': 'ITB',
-'26': 'IIT Kanpur',
-'66': 'Tianjin U',
-'64': 'CUHK',
-'18': 'Fuzhou U',
-'47': 'NU Singapo',
-'65': 'U.E.C.',
-'99': 'UP Diliman',
-'32': 'IIUM',
-'98': 'U Tehran',
-'57': 'Sharif UT',
-'110': 'Zhejiang U',
-'91': 'UESTC',
-'58': 'Sichuan U',
-'12': 'CMI',
-'43': 'Nanyang TU',
-'24': 'IIT-Chicag',
-'105': 'U Wisconsi',
-'33': 'Iowa State',
-'94': 'U Minnesot',
-'40': 'Messiah Co',
-'92': 'U Illinois',
-'90': 'U Chicago',
-'102': 'Tulsa',
-'84': 'U Alberta',
-'15': 'Duke',
-'13': 'William & ',
-'52': 'Princeton',
-'10': 'Carnegie M',
-'101': 'U of Toron',
-'104': 'U Waterloo',
-'97': 'USC',
-'62': 'Stanford U',
-'89': 'U Central ',
-'19': 'Georgia Te',
-'22': 'Harvard',
-'39': 'MIT',
-'85': 'U British ',
-'4': 'AUS',
-'88': 'U Cape Tow',
-'20': 'GUC',
-'2': 'Alex Univ',
-'9': 'Cairo-FCI',
-};
-
-
 // Construct a new feed object and its relevant DOM objects.
 function feed(div, properties) {
     // default properties for this object
@@ -163,6 +46,9 @@ function feed(div, properties) {
     this.unique_feed_id = GLOBAL_FEED_ID_COUNTER;
     GLOBAL_FEED_ID_COUNTER++;
 
+    var data = get_json_synchronous("common_data.php");
+    this.TEAMS = data['TEAMS'];
+
     this.div = $(div);
 
     // update the object according to the user-specified properties
@@ -179,6 +65,7 @@ function feed(div, properties) {
     this.initUI      = _feed_initUI;
     this.start       = _feed_start;
     this.pinRow      = _feed_pinRow;
+    this.tweet       = _feed_tweet;
     this.sort        = _feed_sort;
 
     // Set up the user interface, and start the feed
@@ -189,14 +76,22 @@ function feed(div, properties) {
 // "Pin" a row for the control that has been clicked. Copies the row and places
 // it in the pinned container.
 function _feed_pinRow(clickedObject) {
-    var pinned_row = $(clickedObject).parent().clone();
+    var p = $(clickedObject).parents(".feed_row");
+    var pinned_row = p.clone();
     var control = pinned_row.find("div.feed_row_pin_control");
     var self = this;
     control.text("unpin");
-    control.click(function() { $(this).parent().remove(); });
+    control.click(function() { $(this).parents(".feed_row").remove(); });
 
     // TODO -- should we detect pinned duplicates and avoid them being pinned?
     this.div.find("div.feed_pinned_rows_container").append(pinned_row);
+}
+
+// Send the row to twitter.
+function _feed_tweet(clickedObject) {
+    var tweeted_row = $(clickedObject).parent();
+    console.log('tweeting');
+    console.log(tweeted_row[0].innerHTML);
 }
 
 // Toggle the paused status of the given feed. Clears the update timeout.
@@ -278,22 +173,22 @@ function _feed_updateWith(rows) {
                 var text = row.text.replace(/#p([A-Za-z])/g, "<a href='problem.php?problem_id=$1'>problem $1</a>");
                 text = text.replace(/#t([0-9]+)/g, 
                         function(match, contents, offset, s) {
-                            var link = "<a href='team_feed.php?team_id=" + contents + "'>" + id_to_school_name[contents] + "</a>";
+                            var link = "<a href='team_feed.php?team_id=" + contents + "'>" + self.TEAMS[contents]['school_short'] + "</a>";
                             return link;
                         });
                 description = "<span class='priority_" + row.priority + "'>" + row.contest_time + ': ' + text + "</span>";
             } else if (this.table == 'edit_activity') {
                 var gitweb_url = '/gitweb/?p=homedirs/.git;a=blob;hb=' + row.git_tag + ';f=team' + row.team_id + "/" + row.path;
                 description = "<a href='problem.php?problem_id=" + row.problem_id + "'>Problem " + row.problem_id.toUpperCase() + "</a> &mdash; " +
-                              "<a href='team_feed.php?team_id=" + row.team_id + "'>" + id_to_school_name[row.team_id] + "</a> &mdash; " +
+                              "<a href='team_feed.php?team_id=" + row.team_id + "'>" + self.TEAMS[row.team_id]['school_short'] + "</a> &mdash; " +
                               "<a href='" + gitweb_url + "'>" + row.path + "</a> &mdash; " + 
                               // "<a href='view_source.php?id=" + row.id + "'>" + row.path + "</a> &mdash; " +
                               row.modify_time;
             } else if (this.table == 'submissions') {
                 var kattis_result_translator = {
                    'AC'  : "Accepted",
-                   '(CE)': "Compile Error",
-                   '(IF)': "Illegal Function",
+                   'CE'  : "Compile Error",
+                   'IF'  : "Illegal Function",
                    'MLE' : "Memory Limit Exceeded",
                    'OLE' : "Output Limit Exceeded",
                    'PE'  : "Presentation Error",
@@ -306,7 +201,7 @@ function _feed_updateWith(rows) {
 
                 description = row.contest_time + ': ' + 
                              "<a href='problem.php?problem_id=" + row.problem_id + "'>Problem " + row.problem_id.toUpperCase() + "</a> &mdash; " +
-                             "<a href='team_feed.php?team_id=" + row.team_id + "'>" + id_to_school_name[row.team_id] + "</a> &mdash; " +
+                             "<a href='team_feed.php?team_id=" + row.team_id + "'>" + self.TEAMS[row.team_id]['school_short'] + "</a> &mdash; " +
                              row.lang_id + " &mdash; " +
                              result;
             } else {
@@ -319,8 +214,10 @@ function _feed_updateWith(rows) {
 
             var htmlDescription = $("<div class='feed_row feed_row_recent'>" +
                     "<div class='feed_row_description'>" + description + "</div>" +
+                    "<div class='feed_row_controls'>" +
                     "<div class='feed_row_pin_control'>pin</div>" +
-                    "</div>");
+                    "<div class='feed_row_tweet_control'>tweet</div>" +
+                    "</div></div>");
             // add data which allows sorting
             for (var key in row) {
                 var val = row[key];
@@ -331,6 +228,7 @@ function _feed_updateWith(rows) {
             
             // enable the row to be pinned
             htmlDescription.find('div.feed_row_pin_control').click(function() { self.pinRow(this); });
+            htmlDescription.find('div.feed_row_tweet_control').click(function() { self.tweet(this); });
             this.div.find('div.feed_rows_container').prepend(htmlDescription);
             this.ids_seen[row.id] = true;
         }
