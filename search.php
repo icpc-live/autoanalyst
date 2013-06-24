ec8ec9a444180edff4c2a9567407d0bd87991cc2
Greg Hamerly <hamerly@cs.baylor.edu>
2013-06-24 16:06:54 -0500

    added ability to search by particular database column

--- search.php
+++ search.php
@@ -5,22 +5,28 @@
 
 $location = "";
 
-$query = $_GET["query"];
-if (preg_match("/^[a-z0-9.]+/", $query)) {
+$query = isset($_GET["query"]) ? $_GET["query"] : null;
+$type = isset($_GET["type"]) ? $_GET["type"] : null;
+if (preg_match("/^[a-zA-Z0-9.]+/", $query)) {
     if (preg_match("/^[0-9]+$/", $query)) {
-        $location = "team_feed.php?team_id=$query";
+        $location = "team.php?team_id=$query";
     } else if (preg_match("/^[A-Z]$/i", $query)) {
         $location = "problem.php?problem_id=$query";
     } else {
         # assume they're searching for a team name
-        include('icat.php');
+        require_once 'icat.php';
         $db = init_db();
-        $result = mysql_query("select * from teams where school_name like '%$query%' or school_short like '%$query%' or country like '%$query%' order by school_name", $db);
+        if ($type && preg_match("/^(school_name|school_short|country)$/", $type)) {
+            $query = "select * from teams where " . $type . " like '%$query%' order by school_name";
+        } else {
+            $query = "select * from teams where school_name like '%$query%' or school_short like '%$query%' or country like '%$query%' order by school_name";
+        }
+        $result = mysql_query($query, $db);
 
         $location = array();
         while ($result && ($row = mysql_fetch_assoc($result))) {
-            $school_name = utf8_encode($row["school_name"]);
-            $location[] = array("school_name" => $school_name, "url" => "team_feed.php?team_id=" .  $row["id"]);
+            $school_name = $row["school_name"] . " (" . $row["country"] . ")";
+            $location[] = array("school_name" => $school_name, "url" => "team.php?team_id=" .  $row["id"]);
         }
     }
 }
