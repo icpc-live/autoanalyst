844e2c3ac7b39070114e8d81f1012357013e263c
Jaap Eldering <eldering@a-eskwadraat.nl>
2013-06-29 18:34:41 +0100

    Simplify adding team ID leading zero's.

--- recordteams/record.sh
+++ recordteams/record.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-id=$1
-if [ $id -lt 10 ]; then
-	id="00$id";
-elif [ $id -lt 100 ]; then
-	id="0$id";
-fi
+id=`printf %03d $1`
 
 ffmpeg -i http://192.168.1.141:60$id -vcodec copy -acodec copy -t 60 /share/recording/team$id-`date +%s`.avi > /dev/null 2> /dev/null
 mplayer /home/analyst10/Sonar_pings.ogg
