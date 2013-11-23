267e32d5559a8848a0660df7a623dbc40704ae82
Jaap Eldering <eldering@a-eskwadraat.nl>
2013-11-23 22:24:05 +0000

    Add code analyzer SQL files.

--- create_icat_instance.sql
+++ create_icat_instance.sql
@@ -200,3 +200,144 @@ DROP VIEW IF EXISTS team_regions;
 CREATE VIEW team_regions AS SELECT * FROM icpc2013_team_regions;
 DROP VIEW IF EXISTS top_coder;
 CREATE VIEW top_coder AS SELECT * FROM icpc2013_top_coder;
+
+
+
+--
+-- Mapping from file team id and path name to problem id.  This records
+-- any decisions the code analyzer makes about what problem each file
+-- represents, and it also includes overrides made by the operator.
+-- Problem_id may be null, to indicate that a particular file should be
+-- ignored.
+--
+-- override : true if this is inserted by the operator, so the script won't change it.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_file_to_problem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `path` varchar(256),
+  `problem_id` varchar(10),
+  `override` tinyint(1),
+  PRIMARY KEY (`id`),
+  INDEX `team_path_ftp_index` (`team_id`, `path`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Just a record of the modification times for files in team's directories.  This will
+-- generally be restricted to source files, but I suppose this wouldn't be required.
+--
+-- modify_time_utc is the modification time, in utc.
+-- modify_time is the minutes since the start of the contest.
+--
+-- there's a reason we're recording both of these, but I (DBS) don't
+-- remember it.  I expect they are both somewhat redundant with the
+-- git_tag, since I think that indicates a particular snapshot time.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_edit_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `path` varchar(256),
+  `modify_time_utc` timestamp,
+  `modify_time` int(11),
+  `line_count` int(11),
+  `git_tag` varchar(30),
+  PRIMARY KEY (`id`),
+  INDEX `team_path_ea_index` (`team_id`, `path`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Mapping from file team id and path name to last utc modification
+-- time.  This really just exists to make updating the previous table
+-- efficient.  We only write a new record for a particular file if it
+-- has changed more recently.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_file_modtime` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `path` varchar(256),
+  `modify_time_utc` timestamp,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Summary of edit activity, by problem ID.  It's a map from team id and
+-- problem id to the last modification of a file for that problem.
+-- This is intended to give a quick report of what each team is working
+-- on.
+--
+-- modify_time_utc is the modification time, in utc.
+
+CREATE TABLE IF NOT EXISTS `icpc2013_edit_latest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `problem_id` varchar(10) NOT NULL,
+  `modify_time_utc` timestamp,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Parameters for the code analyzer.
+--
+-- CONTEST_START: UTC start of contest, YYYY-MM-DD-hh-mm-ss
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_analyzer_parameters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `value` varchar(60),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Map from problem id to a problem name.  We have this elswehere, but
+-- I think it's just php.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_problem_name` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` varchar(10) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Map from problem id to a list of keywords for the problem.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_problem_keywords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` varchar(10) NOT NULL,
+  `keyword` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Per-team list of strings to strip, if a team is using one
+-- or more special strings as part of their filenames.
+--
+
+CREATE TABLE IF NOT EXISTS `icpc2013_team_strips` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `str` varchar(30),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+--
+-- Create views for the current contest
+--
+
+CREATE VIEW file_to_problem AS SELECT * FROM icpc2013_file_to_problem;
+CREATE VIEW edit_activity AS SELECT * FROM icpc2013_edit_activity;
+CREATE VIEW file_modtime AS SELECT * FROM icpc2013_file_modtime;
+CREATE VIEW edit_latest AS SELECT * FROM icpc2013_edit_latest;
+CREATE VIEW analyzer_parameters AS SELECT * FROM icpc2013_analyzer_parameters;
+CREATE VIEW problem_name AS SELECT * FROM icpc2013_problem_name;
+CREATE VIEW problem_keywords AS SELECT * FROM icpc2013_problem_keywords;
+CREATE VIEW team_strips AS SELECT * FROM icpc2013_team_strips;
+CREATE VIEW edit_activity_problem AS SELECT edit_activity.*, file_to_problem.problem_id
+    FROM edit_activity LEFT JOIN file_to_problem ON (edit_activity.team_id = file_to_problem.team_id AND edit_activity.path = file_to_problem.path)
+    WHERE file_to_problem.problem_id IS NOT NULL AND file_to_problem.problem_id != 'none';
