1c72d5d13d5cab0598a10b109d575aae63626d79
Anders Sjöqvist <anders@sjoqvi.st>
2015-05-18 15:44:57 +0100

    Insert into 'entries' using prepared statements, with submission ID

--- www/insert_entry.php
+++ www/insert_entry.php
@@ -5,22 +5,28 @@ $date          =& $_REQUEST["date"];
 $user          =& $_REQUEST["user"];
 $priority      =& $_REQUEST["priority"];
 $text          =& $_REQUEST["text"];
+if (isset($_REQUEST["submission"])) {
+    $sid       =  intval($_REQUEST["submission"]);
+} else {
+    $sid       =  null;
+}
 
 require_once 'icat.php';
 $db = init_db();
 
 $_SESSION['entry_username'] = $user;
 
-$result = mysqli_query($db,
-    "insert into entries (contest_time, user, priority, text) values " .
-    sprintf("((SELECT MAX(contest_time) AS last_submission FROM submissions), '%s', %d, '%s')",
-    mysqli_escape_string($db, $user), $priority, mysqli_escape_string($db, $text))
-);
+$stmt = mysqli_prepare($db, 'INSERT INTO entries (contest_time, user, priority, text, submission_id) VALUES ((SELECT MAX(contest_time) AS last_submission FROM submissions), ?, ?, ?, ?)');
 
-if ($result) {
+mysqli_stmt_bind_param($stmt, 'sisi', $user, $priority, $text, $sid);
+
+mysqli_stmt_execute($stmt);
+
+$error = mysqli_stmt_error($stmt);
+if ($error === '') {
     print("okay");
 } else {
-    print("error: " . mysqli_error($db));
+    print("error: " . $error);
 }
 
-?>
+mysqli_stmt_close($stmt);
