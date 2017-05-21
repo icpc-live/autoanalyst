icpc-live/autoanalyst

commit b291676b116cca14549446780ea55abb86e648ec

Make contest 'start_time' a Unix Epoch seconds integer to prevent any confusion about timezones (since the SQL timestamp return is a local time in the SQL server timezone). Note that contest start_time can be NULL when the contest start is being delayed. Also rename 'modify_time_utc' to 'modify_timestamp' and use Unix Epoch seconds there.

code_analyzer/analyzer.py
@@ -124,7 +124,7 @@ class Analyzer:
 
         # Contest start time in Unix seconds from the database.
         cursor = dbConn.cursor()
-        cursor.execute( "SELECT UNIX_TIMESTAMP(start_time) FROM contests ORDER BY start_time DESC LIMIT 1" )
+        cursor.execute( "SELECT start_time FROM contests ORDER BY start_time DESC LIMIT 1" )
         row = cursor.fetchone()
         if ( row == None ):
             print("Error: no contest found in the database.")
@@ -172,11 +172,10 @@ class Analyzer:
             row = cursor.fetchone()
 
         # get latest known edit times for every team/path
-        cursor.execute( "SELECT id, team_id, path, modify_time_utc FROM file_modtime" )
+        cursor.execute( "SELECT id, team_id, path, modify_timestamp FROM file_modtime" )
         row = cursor.fetchone()
         while ( row != None ):
-            t = int( calendar.timegm( row[ 3 ].timetuple() ) )
-            self.lastEditTimes[ ( row[ 1 ], row[ 2 ] ) ] = [ row[ 0 ], t, None ]
+            self.lastEditTimes[ ( row[ 1 ], row[ 2 ] ) ] = [ row[ 0 ], row[ 3 ], None ]
 
             row = cursor.fetchone()
         
@@ -528,20 +527,19 @@ class Analyzer:
         cursor = dbConn.cursor()
         for k, v in self.lastEditTimes.iteritems():
             if v[ 2 ] != None:
-                tstr = time.strftime( "%Y-%m-%d %H:%M:%S", time.gmtime( v[ 2 ].time ) )
-
+                t = time.gmtime( v[ 2 ].time )
                 if v[ 0 ] == None:
-                    update = "INSERT INTO file_modtime (team_id, path, modify_time_utc ) VALUES ( '%s', '%s', '%s' )" % ( k[ 0 ], dbConn.escape_string( k[ 1 ] ), tstr )
+                    update = "INSERT INTO file_modtime (team_id, path, modify_timestamp ) VALUES ( '%s', '%s', '%d' )" % ( k[ 0 ], dbConn.escape_string( k[ 1 ] ), t )
                 
                     cursor.execute( update )
                 else:
-                    update = "UPDATE file_modtime SET modify_time_utc='%s' WHERE id='%d'" % ( tstr, v[ 0 ] )
+                    update = "UPDATE file_modtime SET modify_time='%d' WHERE id='%d'" % ( t, v[ 0 ] )
                     cursor.execute( update )
 
                 # Compute time since start of contest.
                 cmin = ( v[ 2 ].time - self.contestStart ) / 60
 
-                update = "INSERT INTO edit_activity (team_id, path, modify_time_utc, modify_time, file_size_bytes, line_count, lines_changed, git_tag ) VALUES ( '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%s' )" % ( k[ 0 ], dbConn.escape_string( k[ 1 ] ), tstr, cmin, v[ 2 ].size, v[ 2 ].lineCount, v[ 2 ].linesChanged, tag )
+                update = "INSERT INTO edit_activity (team_id, path, modify_timestamp, modify_time, file_size_bytes, line_count, lines_changed, git_tag ) VALUES ( '%s', '%s', '%d', '%s', '%d', '%d', '%d', '%s' )" % ( k[ 0 ], dbConn.escape_string( k[ 1 ] ), t, cmin, v[ 2 ].size, v[ 2 ].lineCount, v[ 2 ].linesChanged, tag )
                 
                 cursor.execute( update )
                 
@@ -556,11 +554,10 @@ class Analyzer:
         modLatest = {}
 
         # get latest known edit times for every team/problem.
-        cursor.execute( "SELECT id, team_id, problem_id, modify_time_utc FROM edit_latest" )
+        cursor.execute( "SELECT id, team_id, problem_id, modify_timestamp FROM edit_latest" )
         row = cursor.fetchone()
         while ( row != None ):
-            t = int( calendar.timegm( row[ 3 ].timetuple() ) )
-            modLatest[ ( row[ 1 ], row[ 2 ] ) ] = [ row[ 0 ], t, 0 ]
+            modLatest[ ( row[ 1 ], row[ 2 ] ) ] = [ row[ 0 ], row[ 3 ], 0 ]
             row = cursor.fetchone()
         
         for k, v in self.fileMappings.iteritems():
@@ -584,13 +581,13 @@ class Analyzer:
                         modLatest[ ( k[ 0 ], prob ) ] = [ None, t, 1 ]
 
         for k, v in modLatest.iteritems():
-            tstr = time.strftime( "%Y-%m-%d %H:%M:%S", time.gmtime( v[ 1 ] ) )
+            t = time.gmtime( v[ 1 ] )
             if v[ 0 ] == None:
-                update = "INSERT INTO edit_latest (team_id, problem_id, modify_time_utc ) VALUES ( '%s', '%s', '%s' )" % ( k[ 0 ], k[ 1 ], tstr )
+                update = "INSERT INTO edit_latest (team_id, problem_id, modify_timestamp ) VALUES ( '%s', '%s', '%d' )" % ( k[ 0 ], k[ 1 ], t )
                 
                 cursor.execute( update )
             elif v[ 2 ]:
-                update = "UPDATE edit_latest SET modify_time_utc='%s' WHERE id='%d'" % ( tstr, v[ 0 ] )
+                update = "UPDATE edit_latest SET modify_timestamp='%d' WHERE id='%d'" % ( t, v[ 0 ] )
                 cursor.execute( update )
             else:
                 update = "DELETE FROM edit_latest WHERE id='%d'" % ( v[ 0 ] )

code_analyzer/githomes.py
@@ -95,7 +95,7 @@ class GitHomes:
 
         # figure out a start time in the format we will get from the CDS.
         cursor = dbConn.cursor()
-        cursor.execute( "SELECT UNIX_TIMESTAMP(start_time) FROM contests ORDER BY start_time DESC LIMIT 1" )
+        cursor.execute( "SELECT start_time FROM contests ORDER BY start_time DESC LIMIT 1" )
         row = cursor.fetchone()
         if ( row == None ):
             print("Error: no contest found in the database.")

create_icat_instance.sql
@@ -45,7 +45,7 @@ DROP TABLE IF EXISTS `contests`;
 CREATE TABLE IF NOT EXISTS `contests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `contest_name` varchar(150) NOT NULL,
-  `start_time` datetime COMMENT 'Contest start time in UTC.',
+  `start_time` int(11) DEFAULT NULL COMMENT 'Contest start time as Unix Epoch seconds.',
   `length` int(11) DEFAULT NULL COMMENT 'Contest length in seconds.',
   `freeze` int(11) DEFAULT NULL COMMENT 'Seconds into contest when scoreboard is frozen.',
   PRIMARY KEY (`id`)
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `contests` (
 -- directories.  This will generally be restricted to source files,
 -- but I suppose this wouldn't be required.
 --
--- modify_time_utc is the modification time, in utc.
+-- modify_timestamp is the modification time, in Unix Epoch.
 -- modify_time is the minutes since the start of the contest.
 --
 -- there's a reason we're recording both of these, but I (DBS) don't
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `edit_activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `team_id` int(11) NOT NULL,
   `path` varchar(256) DEFAULT NULL,
-  `modify_time_utc` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modify_timestamp` int(11) DEFAULT NULL,
   `modify_time` int(11) DEFAULT NULL,
   `line_count` int(11) DEFAULT NULL,
   `file_size_bytes` int(11) DEFAULT NULL,
@@ -96,14 +96,14 @@ CREATE TABLE IF NOT EXISTS `edit_activity` (
 -- This is intended to give a quick report of what each team is working
 -- on.
 --
--- modify_time_utc is the modification time, in utc.
+-- modify_timestamp is the modification time, in Unix Epoch.
 
 DROP TABLE IF EXISTS `edit_latest`;
 CREATE TABLE IF NOT EXISTS `edit_latest` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `team_id` int(11) NOT NULL,
   `problem_id` varchar(10) NOT NULL,
-  `modify_time_utc` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modify_timestamp` int(110 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
@@ -142,8 +142,8 @@ CREATE TABLE IF NOT EXISTS `facts` (
 --
 
 --
--- Mapping from file team id and path name to last utc modification
--- time.  This really just exists to make updating the previous table
+-- Mapping from file team id and path name to last modification
+-- timestamp.  This really just exists to make updating the previous table
 -- efficient.  We only write a new record for a particular file if it
 -- has changed more recently.
 --
@@ -153,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `file_modtime` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `team_id` int(11) NOT NULL,
   `path` varchar(256) DEFAULT NULL,
-  `modify_time_utc` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modify_timestamp` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 

www/feed.js
@@ -258,7 +258,7 @@ function _feed_updateWith(rows) {
                               "<a href='team.php?team_id=" + row.team_id + "'>" + self.teams[row.team_id]['school_short'] + "</a> (#t" + row.team_id + ") &mdash; " +
                               "<a href='" + gitweb_url + "'>" + row.path + "</a> &mdash; " + 
                               row.modify_time + 
-                              "<span class='feed_timestamp' timestamp='" + row.modify_time_utc + "'></span>";
+                              "<span class='feed_timestamp' timestamp='" + row.modify_timestamp + "'></span>";
             } else if (this.table == 'submissions') {
                 var is_accepted = (row.result == 'AC') ? 'kattis_result_accepted' : 'kattis_result_not_accepted';
                 var result = "<span class='" + is_accepted + "'>" + self.judgements[row.result].label_long + "</span>";

commit 1c0e566ffb8d0cdf48985d27dd4cb406663f030c

Fix a time conversion bug introduced in b291676b116cca14549446780ea55abb86e648ec

code_analyzer/analyzer.py
@@ -527,7 +527,7 @@ class Analyzer:
         cursor = dbConn.cursor()
         for k, v in self.lastEditTimes.iteritems():
             if v[ 2 ] != None:
-                t = time.gmtime( v[ 2 ].time )
+                t = v[ 2 ].time
                 if v[ 0 ] == None:
                     update = "INSERT INTO file_modtime (team_id, path, modify_timestamp ) VALUES ( '%s', '%s', '%d' )" % ( k[ 0 ], dbConn.escape_string( k[ 1 ] ), t )
                 
@@ -581,7 +581,7 @@ class Analyzer:
                         modLatest[ ( k[ 0 ], prob ) ] = [ None, t, 1 ]
 
         for k, v in modLatest.iteritems():
-            t = time.gmtime( v[ 1 ] )
+            t = v[ 1 ]
             if v[ 0 ] == None:
                 update = "INSERT INTO edit_latest (team_id, problem_id, modify_timestamp ) VALUES ( '%s', '%s', '%d' )" % ( k[ 0 ], k[ 1 ], t )
                 