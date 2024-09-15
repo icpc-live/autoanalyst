53040f6b8be8678248ce9cc199f663d4d1c004b0
Yakov Dlougach <yakov.dlougach@wayve.ai>
2024-09-15 17:18:03 +0500

    Wait better in human messages

--- katalyze/src/main/kotlin/katalyzeapp/HumanMessages.kt
+++ katalyze/src/main/kotlin/katalyzeapp/HumanMessages.kt
@@ -8,17 +8,17 @@ import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.selects.onTimeout
 import kotlinx.coroutines.selects.whileSelect
-import kotlinx.datetime.Clock
 import model.Commentary
 import model.EventImportance
 import model.dsl.v1.Entries
 import org.icpclive.cds.api.ContestInfo
-import org.icpclive.cds.api.toTeamId
+import org.icpclive.cds.api.ContestStatus
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.neq
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.time.Duration.Companion.ZERO
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
@@ -32,23 +32,31 @@ fun getHumanMessagesFromDatabase(
     scope.produce(capacity = UNLIMITED) {
         val seenIds = mutableSetOf<Int>();
         while (true) {
-            val contestInfo = contestStateTracker.state?.info
-            if (contestInfo == null) {
-                kotlinx.coroutines.delay(1.seconds)
+            var contestInfo = contestStateTracker.state?.info
+            if (contestInfo == null || contestInfo.teams.isEmpty()) {
+                kotlinx.coroutines.delay(10.milliseconds)
                 continue
             }
             val newEntries = transaction(db) {
                 val entries =
-                    Entries.selectAll().where(Entries.user.neq(Commentary.KATALYZER_USER)).orderBy(Entries.date).toList()
+                    Entries.selectAll().where(Entries.user.neq(Commentary.KATALYZER_USER)).orderBy(Entries.date)
+                        .toList()
                 entries.filter { entry ->
-                    (entry[Entries.id] !in seenIds).also {
-                        if (it) {
-                            seenIds.add(entry[Entries.id])
-                        }
-                    }
+                    (entry[Entries.id] !in seenIds)
+                }.onEach { entry ->
+                    seenIds.add(entry[Entries.id])
                 }
             }
-            newEntries.forEach { entry -> send(commentaryFromDBRow(entry, contestInfo)) }
+            newEntries.forEach { entry ->
+                if (entry[Entries.contestTime] >= 0) {
+                    while (contestInfo?.status is ContestStatus.BEFORE) {
+                        kotlinx.coroutines.delay(10.milliseconds)
+                        contestInfo = contestStateTracker.state?.info
+                    }
+                }
+                val commentary = commentaryFromDBRow(entry, contestInfo!!)
+                send(commentary)
+            }
             kotlinx.coroutines.delay(2.seconds)
         }
     }
